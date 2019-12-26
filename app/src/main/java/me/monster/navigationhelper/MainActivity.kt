b3423dd16283c40bef7977f5d4240e119910892d
jiangjiwei <jiangjiwei0322@outlook.com>
2019-12-26 14:22:24 +0800

    demo: use keep_state_fragment for Demo

--- app/src/main/java/me/monster/navigationhelper/MainActivity.kt
+++ app/src/main/java/me/monster/navigationhelper/MainActivity.kt
@@ -2,11 +2,20 @@ package me.monster.navigationhelper
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import me.monster.navigationhelper.lib.KeepStateNavigator
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.Navigation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = Navigation.findNavController(this, R.id.fragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment?
+        val navigator = KeepStateNavigator(this, navHostFragment!!.childFragmentManager, R.id.fragment)
+        navController.navigatorProvider.addNavigator(navigator)
+        navController.setGraph(R.navigation.demo_navigation)
     }
 }
